0bfa73c7029ebcbfcea89284d904f07d0ccffa20
heyu2019 <615791941@qq.com>
2020-06-05 17:19:30 +0800

    deploy

---
+++ precache-manifest.431bf6de17ee6e90887ae6298131cbe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb3b1298cb9d0a7755d",
+    "url": "/YuPocketbook/css/app.afcd026e.css"
+  },
+  {
+    "revision": "8ac9224c9831d4a88f95fffbe3d81ef1",
+    "url": "/YuPocketbook/index.html"
+  },
+  {
+    "revision": "4bb3b1298cb9d0a7755d",
+    "url": "/YuPocketbook/js/app.39ac59a2.js"
+  },
+  {
+    "revision": "135ad3bb3647aed87151",
+    "url": "/YuPocketbook/js/chunk-vendors.d060d046.js"
+  },
+  {
+    "revision": "c683893f3b41ba40a8c61ce80a03181c",
+    "url": "/YuPocketbook/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/YuPocketbook/robots.txt"
+  },
+  {
+    "revision": "3f4099829273050ab950c3f132429abb",
+    "url": "/YuPocketbook/scanCode.png"
+  }
+]);
